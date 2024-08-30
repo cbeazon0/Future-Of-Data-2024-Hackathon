8bc8a48b1c3c33398c0a5bf79f4c9188735258f4
muhlenlogan8 <muhlenlogan8@gmail.com>
2024-08-30 01:00:01 -0400

    Began revamping app for tailwind & Modularization

---
+++ frontend/src/NotUsed/App.jsx
@@ -0,0 +1,43 @@
+import { useState, useEffect } from "react"
+import "./App.css"
+import axios from "axios"
+import Hero from "./Hero"
+import Header from "./Header"
+import Section from "./Section"
+
+function App() {
+  const [users, setUsers] = useState([]) // Create a state variable to store and change the users array
+
+  // API calls are asynchronous, so we need to use async/await
+  const fetchAPI = async () => {
+    // Fetch data from the API using axios
+    const response = await axios.get("http://localhost:5000/api/users");
+    // Log the data to the console, specifically the users array
+    console.log(response.data.users);
+    // Set the users state variable to the users array from the API
+    setUsers(response.data.users);
+  };
+
+  useEffect(() => {
+    // Call the fetchAPI function when the component is mounted
+    fetchAPI();
+  }, []); // The empty array ensures that the function is only called once on initial render
+
+  return (
+    <>
+      <Header />
+      <Hero />
+      {/* <Section /> */}
+      {/* <div id="content">
+        {users.map((user, index) => (
+            <div key={index}> 
+              <span>{user}</span>
+              <br />
+            </div>
+        ))}
+      </div> */}
+    </>
+  );
+}
+
+export default App
